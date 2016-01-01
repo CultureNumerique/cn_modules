115aa7e4329b61436e6f0cae63be8de875ae60f9
Marc Tommasi <marc.tommasi@inria.fr>
2016-01-01 15:59:28 +0100

    petite modif pour le depot en staging

--- src/toStagging.sh
+++ src/toStagging.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
 #rsync -avh --delete build vduhal@culturenumerique.univ-lille3.fr:htdocs/staging
-scp  -r build vduhal@culturenumerique.univ-lille3.fr:htdocs/staging
+
+source='build'
+dest=vduhal@culturenumerique.univ-lille3.fr:htdocs/staging/
+
+if [ $# -ge 2 ]; then
+    echo "$0 [source]"
+    echo "Copy the source dir (default: build) into the destination $dest " 
+    exit 0
+else
+    if [ $# -eq 1 ]; then
+	source=$1
+    fi
+fi
+
+if [ -d $source ]; then
+    scp  -v -r -C $source $dest
+else
+    echo "Unable to find: " $source
+fi
+   
 
